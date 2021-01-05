119c6e7ae7eeee17c1d2cb848702fda563554b7b
Piyush Singhania <69912046+Piyush-Singhania@users.noreply.github.com>
2021-01-05 18:12:11 +0530

    version 3.2.2
    1). Adding some lines of javascript in script.js file so to fix a bug.
    The bug was basically =>Earlier a hovering effect was added via css in two buttons (like and hire us) in the Hire us section. Which was not properly working as the buttons did not changed the color at the same time while hoveing. So removed those css lines and solved it via javascript.
    2). Also added some more lines so as to meet the desired input in forms section for both the categories (Employer, Job Seeker).

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//SECTION TO DISPLAY THE NUMBER OF TIMES WE ARE CONTACTED
+
 let count = 0;
 var counterPhoneCalls = document.getElementsByClassName("counter")[0];
 setInterval(() => {
@@ -33,3 +35,58 @@ setInterval(() => {
     counterCupCoffee.innerHTML = `${count4}`;
   }
 }, 1.6);
+
+// FORMS SECTION
+
+var employerBtn = document.querySelector("#option-1");
+var jobSeekerBtn = document.querySelector("#option-2");
+var numberOrNameInput = document.querySelector("#nameOrNumber");
+
+employerBtn.addEventListener("click", () => {
+  employerBtn.style.backgroundColor = "#e01631";
+  employerBtn.style.color = "white";
+  numberOrNameInput.placeholder = "Company Name";
+  jobSeekerBtn.style.removeProperty("background-color");
+  jobSeekerBtn.style.color = "rgb(57, 57, 139)";
+});
+
+jobSeekerBtn.addEventListener("click", () => {
+  console.log("Ayush");
+  jobSeekerBtn.style.backgroundColor = "#2a2a68";
+  jobSeekerBtn.style.color = "white";
+  numberOrNameInput.placeholder = "Phone Number";
+  employerBtn.style.backgroundColor = "white";
+  employerBtn.style.color = "rgb(57, 57, 139)";
+});
+
+// HIRE US SECTION
+var likeBtn = document.querySelector("#container-4-btn-1");
+var hireUsBtn = document.querySelector("#container-4-btn-2");
+
+likeBtn.addEventListener("mouseover", () => {
+  likeBtn.style.backgroundColor = "#571f9c";
+  likeBtn.style.color = "white";
+  hireUsBtn.style.backgroundColor = "#571f9c";
+  hireUsBtn.style.color = "white";
+});
+
+likeBtn.addEventListener("mouseout", () => {
+  likeBtn.style.backgroundColor = "";
+  likeBtn.style.color = "";
+  hireUsBtn.style.backgroundColor = "";
+  hireUsBtn.style.color = "";
+});
+
+hireUsBtn.addEventListener("mouseover", () => {
+  likeBtn.style.backgroundColor = "#571f9c";
+  likeBtn.style.color = "white";
+  hireUsBtn.style.backgroundColor = "#571f9c";
+  hireUsBtn.style.color = "white";
+});
+
+hireUsBtn.addEventListener("mouseout", () => {
+  likeBtn.style.backgroundColor = "";
+  likeBtn.style.color = "";
+  hireUsBtn.style.backgroundColor = "";
+  hireUsBtn.style.color = "";
+});
