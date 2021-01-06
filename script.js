a17e15a90eff6bdd1c27d8919445cf51d778edb2
Piyush Singhania <69912046+Piyush-Singhania@users.noreply.github.com>
2021-01-06 21:13:22 +0530

    version 3.2.3
    Adding some lines of code to solve an earlier bug.
    Now the rolling numbers effect fire only at the time when the user scrolls to that section of webpage for the first time.

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
 //SECTION TO DISPLAY THE NUMBER OF TIMES WE ARE CONTACTED
 
-let count = 0;
-var counterPhoneCalls = document.getElementsByClassName("counter")[0];
-setInterval(() => {
-  if (count < 500) {
-    count = count + 1;
-    counterPhoneCalls.innerHTML = `${count}M`;
-  }
-}, 1);
+window.addEventListener("scroll", function () {
+  var place = document.scrollingElement.scrollTop;
+  var alertOn = document.getElementById("container-7").offsetTop;
+  console.log(place);
+  console.log(alertOn);
+  if (place + 250 > alertOn) {
+    this.removeEventListener("scroll", arguments.callee, false);
 
-let count2 = 18500;
-var counterCostAndTime = document.getElementsByClassName("counter")[1];
-setInterval(() => {
-  if (count2 < 19000) {
-    count2 = count2 + 1;
-    counterCostAndTime.innerHTML = `${count2}`;
-  }
-}, 1.2);
+    let count = 0;
+    var counterPhoneCalls = document.getElementsByClassName("counter")[0];
+    setInterval(() => {
+      if (count < 500) {
+        count = count + 1;
+        counterPhoneCalls.innerHTML = `${count}M`;
+      }
+    }, 1);
 
-let count3 = 1831;
-var counterStatsBar = document.getElementsByClassName("counter")[2];
-setInterval(() => {
-  if (count3 < 2331) {
-    count3 = count3 + 1;
-    counterStatsBar.innerHTML = `${count3}`;
-  }
-}, 1.4);
+    let count2 = 18500;
+    var counterCostAndTime = document.getElementsByClassName("counter")[1];
+    setInterval(() => {
+      if (count2 < 19000) {
+        count2 = count2 + 1;
+        counterCostAndTime.innerHTML = `${count2}`;
+      }
+    }, 1.2);
 
-let count4 = 111760;
-var counterCupCoffee = document.getElementsByClassName("counter")[3];
-setInterval(() => {
-  if (count4 < 112260) {
-    count4 = count4 + 1;
-    counterCupCoffee.innerHTML = `${count4}`;
+    let count3 = 1831;
+    var counterStatsBar = document.getElementsByClassName("counter")[2];
+    setInterval(() => {
+      if (count3 < 2331) {
+        count3 = count3 + 1;
+        counterStatsBar.innerHTML = `${count3}`;
+      }
+    }, 1.4);
+
+    let count4 = 111760;
+    var counterCupCoffee = document.getElementsByClassName("counter")[3];
+    setInterval(() => {
+      if (count4 < 112260) {
+        count4 = count4 + 1;
+        counterCupCoffee.innerHTML = `${count4}`;
+      }
+    }, 1.6);
   }
-}, 1.6);
+});
 
 // FORMS SECTION
 
