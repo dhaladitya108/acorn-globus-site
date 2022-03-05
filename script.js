eeef0a4c91d8c3ab422d7244fd067344d6b13eb0
Piyush Singhania <piyushsinghania28@gmail.com>
2022-03-05 18:05:57 +0530

    Added content for alt tags

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //SECTION TO DISPLAY THE NUMBER OF TIMES WE ARE CONTACTED
 
-window.addEventListener("scroll", function () {
+window.addEventListener('scroll', function () {
   var place = document.scrollingElement.scrollTop;
-  var alertOn = document.getElementById("container-7").offsetTop;
+  var alertOn = document.getElementById('container-7').offsetTop;
   // console.log(place);
   // console.log(alertOn);
   if (place + 250 > alertOn) {
-    this.removeEventListener("scroll", arguments.callee, false);
+    this.removeEventListener('scroll', arguments.callee, false);
 
     let count = 0;
-    var counterPhoneCalls = document.getElementsByClassName("counter")[0];
+    var counterPhoneCalls = document.getElementsByClassName('counter')[0];
     setInterval(() => {
       if (count < 500) {
         count = count + 1;
@@ -18,7 +18,7 @@ window.addEventListener("scroll", function () {
     }, 1);
 
     let count2 = 8500;
-    var counterCostAndTime = document.getElementsByClassName("counter")[1];
+    var counterCostAndTime = document.getElementsByClassName('counter')[1];
     setInterval(() => {
       if (count2 < 9000) {
         count2 = count2 + 1;
@@ -27,7 +27,7 @@ window.addEventListener("scroll", function () {
     }, 1.2);
 
     let count3 = 831;
-    var counterStatsBar = document.getElementsByClassName("counter")[2];
+    var counterStatsBar = document.getElementsByClassName('counter')[2];
     setInterval(() => {
       if (count3 < 1331) {
         count3 = count3 + 1;
@@ -36,7 +36,7 @@ window.addEventListener("scroll", function () {
     }, 1.4);
 
     let count4 = 1760;
-    var counterCupCoffee = document.getElementsByClassName("counter")[3];
+    var counterCupCoffee = document.getElementsByClassName('counter')[3];
     setInterval(() => {
       if (count4 < 2260) {
         count4 = count4 + 1;
@@ -74,35 +74,35 @@ jobSeekerBtn.addEventListener("click", () => {
 */
 
 // CTA SECTION
-var likeBtn = document.querySelector("#container-4-btn-1");
-var hireUsBtn = document.querySelector("#container-4-btn-2");
+var likeBtn = document.querySelector('#container-4-btn-1');
+var hireUsBtn = document.querySelector('#container-4-btn-2');
 
-likeBtn.addEventListener("mouseover", () => {
-  likeBtn.style.backgroundColor = "#571f9c";
-  likeBtn.style.color = "white";
-  hireUsBtn.style.backgroundColor = "#571f9c";
-  hireUsBtn.style.color = "white";
+likeBtn.addEventListener('mouseover', () => {
+  likeBtn.style.backgroundColor = '#571f9c';
+  likeBtn.style.color = 'white';
+  hireUsBtn.style.backgroundColor = '#571f9c';
+  hireUsBtn.style.color = 'white';
 });
 
-likeBtn.addEventListener("mouseout", () => {
-  likeBtn.style.backgroundColor = "";
-  likeBtn.style.color = "";
-  hireUsBtn.style.backgroundColor = "";
-  hireUsBtn.style.color = "";
+likeBtn.addEventListener('mouseout', () => {
+  likeBtn.style.backgroundColor = '';
+  likeBtn.style.color = '';
+  hireUsBtn.style.backgroundColor = '';
+  hireUsBtn.style.color = '';
 });
 
-hireUsBtn.addEventListener("mouseover", () => {
-  likeBtn.style.backgroundColor = "#571f9c";
-  likeBtn.style.color = "white";
-  hireUsBtn.style.backgroundColor = "#571f9c";
-  hireUsBtn.style.color = "white";
+hireUsBtn.addEventListener('mouseover', () => {
+  likeBtn.style.backgroundColor = '#571f9c';
+  likeBtn.style.color = 'white';
+  hireUsBtn.style.backgroundColor = '#571f9c';
+  hireUsBtn.style.color = 'white';
 });
 
-hireUsBtn.addEventListener("mouseout", () => {
-  likeBtn.style.backgroundColor = "";
-  likeBtn.style.color = "";
-  hireUsBtn.style.backgroundColor = "";
-  hireUsBtn.style.color = "";
+hireUsBtn.addEventListener('mouseout', () => {
+  likeBtn.style.backgroundColor = '';
+  likeBtn.style.color = '';
+  hireUsBtn.style.backgroundColor = '';
+  hireUsBtn.style.color = '';
 });
 
 // scroll to contact form
@@ -126,33 +126,79 @@ var app = new Vue({
       //   description: "Open source contribution on Frappe Books. <Br /> A free Desktop book-keeping software for small-businesses and freelancers."
       // },
       {
-        title: "Eitoss",
-        link: "https://eitoss.com/",
-        role: "SOFTWARE CONSULTANT",
-        tags: ["Remote Work"],
-        filename: "eitoss.png",
-        stack: ["vue.svg", "nodejs.png", "javascript.png"],
-        description: "Eitoss is a communication tool for managers and frontline workers. It builds on the concept of Kaizen and help companies build product that are driven by the people who are on the frontline."
+        title: 'Eitoss',
+        link: 'https://eitoss.com/',
+        role: 'SOFTWARE CONSULTANT',
+        tags: ['Remote Work'],
+        image: {
+          filename: 'eitoss.png',
+          alt: 'Eitoss'
+        },
+        stack: [
+          {
+            img: 'vue.svg',
+            alt: 'vue',
+          },
+          {
+            img: 'nodejs.png',
+            alt: 'nodejs',
+          },
+          {
+            img: 'javascript.png',
+            alg: 'javascript',
+          },
+        ],
+        description:
+          'Eitoss is a communication tool for managers and frontline workers. It builds on the concept of Kaizen and help companies build product that are driven by the people who are on the frontline.',
       },
       {
-        title: "Rumie",
-        link: "https://about.rumie.org/",
-        role: "SENIOR FRONTEND ENGINEER",
-        tags: ["Project"],
-        filename: "rumie-pic.png",
-        stack: ["vue.svg", "javascript.png"],
-        description: "Rumie is a non-profit that makes access to free digital education possible for underserved communities worldwide. We collaborated on revamping the experience for application content managers. The new app built using Vue provides a fast and intuitive way to deal with complex flows."
+        title: 'Rumie',
+        link: 'https://about.rumie.org/',
+        role: 'SENIOR FRONTEND ENGINEER',
+        tags: ['Project'],
+        image: {
+          filename: 'rumie-pic.png',
+          alt: 'Rumie'
+        },
+        stack: [
+          {
+            img: 'vue.svg',
+            alt: 'vue',
+          },
+          {
+            img: 'javascript.png',
+            alg: 'javascript',
+          },
+        ],
+        description:
+          'Rumie is a non-profit that makes access to free digital education possible for underserved communities worldwide. We collaborated on revamping the experience for application content managers. The new app built using Vue provides a fast and intuitive way to deal with complex flows.',
       },
       {
-        title: "Formester",
-        link: "https://formester.com/",
-        role: "FULL STACK DEVELOPER",
-        tags: ["Full time"],
-        filename: "formester-pic.png",
-        stack: ["vue.svg", "javascript.png", "rails-logo.png"],
-        description: "A form backend as a service. It makes creating form endpoints for websites very easy. It requires zero programming knowledge. The user has the full control over the look and feel of the form. In Formester validation, spam protection and integration with different CRMs are few clicks away."
+        title: 'Formester',
+        link: 'https://formester.com/',
+        role: 'FULL STACK DEVELOPER',
+        tags: ['Full time'],
+        image: {
+          filename: 'formester-pic.png',
+          alt: 'Formester'
+        },
+        stack: [
+          {
+            img: 'vue.svg',
+            alt: 'vue',
+          },
+          {
+            img: 'javascript.png',
+            alg: 'javascript',
+          },
+          {
+            img: 'rails-logo.png',
+            alt: 'Ruby on Rails',
+          },
+        ],
+        description:
+          'A form backend as a service. It makes creating form endpoints for websites very easy. It requires zero programming knowledge. The user has the full control over the look and feel of the form. In Formester validation, spam protection and integration with different CRMs are few clicks away.',
       },
-
-    ]
-  }
-})
+    ],
+  },
+});
