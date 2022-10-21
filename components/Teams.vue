68ac4cca8fa011bc4af348dfabbd5e45d20db99d
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2022-10-21 07:31:03 +0530

    unnecessary code removed n alt tag fixed

--- components/Teams.vue
+++ components/Teams.vue
@@ -3,148 +3,6 @@
     <div class="container">
       <span class="subtext">our team</span>
       <h2 class="subheading">Get to know us closer</h2>
-      <!-- <div class="py-4">
-        <carousel v-bind="options">
-          <slide class="px-2">
-            <div class="team__card">
-              <img
-                src="@/assets/images/team/ankit-img.png"
-                alt="Ankit"
-                class="pr__img"
-              />
-              <h4 class="pr__name mt-4">Ankit</h4>
-              <p class="pr__role">Founder & Partner</p>
-              <div class="socials d-flex mt-3 d-none">
-                <img src="@/assets/images/socials/twitter.svg" alt="Twitter" />
-                <img
-                  src="@/assets/images/socials/linkedin.svg"
-                  alt="Linkedin"
-                />
-                <img src="@/assets/images/socials/github.svg" alt="Github" />
-                <img
-                  src="@/assets/images/socials/instagram.svg"
-                  alt="Instagram"
-                />
-              </div>
-            </div>
-          </slide>
-          <slide class="px-2">
-            <div class="team__card">
-              <img
-                src="@/assets/images/team/tarun.png"
-                alt="Tarun"
-                class="pr__img"
-              />
-              <h4 class="pr__name mt-4">Tarun</h4>
-              <p class="pr__role">Founder & Partner</p>
-              <div class="socials d-flex mt-3 d-none">
-                <img src="@/assets/images/socials/twitter.svg" alt="Twitter" />
-                <img
-                  src="@/assets/images/socials/linkedin.svg"
-                  alt="Linkedin"
-                />
-                <img src="@/assets/images/socials/github.svg" alt="Github" />
-                <img
-                  src="@/assets/images/socials/instagram.svg"
-                  alt="Instagram"
-                />
-              </div>
-            </div>
-          </slide>
-          <slide class="px-2">
-            <div class="team__card">
-              <img
-                src="@/assets/images/team/piyush.png"
-                alt="Ankit"
-                class="pr__img"
-              />
-              <h4 class="pr__name mt-4">Piyush</h4>
-              <p class="pr__role">Developer</p>
-              <div class="socials d-flex mt-3 d-none">
-                <img src="@/assets/images/socials/twitter.svg" alt="Twitter" />
-                <img
-                  src="@/assets/images/socials/linkedin.svg"
-                  alt="Linkedin"
-                />
-                <img src="@/assets/images/socials/github.svg" alt="Github" />
-                <img
-                  src="@/assets/images/socials/instagram.svg"
-                  alt="Instagram"
-                />
-              </div>
-            </div>
-          </slide>
-          <slide class="px-2">
-            <div class="team__card">
-              <img
-                src="@/assets/images/team/santosh.png"
-                alt="Ankit"
-                class="pr__img"
-              />
-              <h4 class="pr__name mt-4">Santosh</h4>
-              <p class="pr__role">Developer</p>
-              <div class="socials d-flex mt-3 d-none">
-                <img src="@/assets/images/socials/twitter.svg" alt="Twitter" />
-                <img
-                  src="@/assets/images/socials/linkedin.svg"
-                  alt="Linkedin"
-                />
-                <img src="@/assets/images/socials/github.svg" alt="Github" />
-                <img
-                  src="@/assets/images/socials/instagram.svg"
-                  alt="Instagram"
-                />
-              </div>
-            </div>
-          </slide>
-          <slide class="px-2">
-            <div class="team__card">
-              <img
-                src="@/assets/images/team/aditya.png"
-                alt="Ankit"
-                class="pr__img"
-              />
-              <h4 class="pr__name mt-4">Aditya</h4>
-              <p class="pr__role">Designer</p>
-              <div class="socials d-flex mt-3 d-none">
-                <img src="@/assets/images/socials/twitter.svg" alt="Twitter" />
-                <img
-                  src="@/assets/images/socials/linkedin.svg"
-                  alt="Linkedin"
-                />
-                <img src="@/assets/images/socials/github.svg" alt="Github" />
-                <img
-                  src="@/assets/images/socials/instagram.svg"
-                  alt="Instagram"
-                />
-              </div>
-            </div>
-          </slide>
-          <slide class="px-2">
-            <div class="team__card">
-              <img
-                src="@/assets/images/team/prashant.png"
-                alt="Prashant"
-                class="pr__img"
-              />
-              <h4 class="pr__name mt-4">Prashant</h4>
-              <p class="pr__role">Full Stack Engineer</p>
-              <div class="socials d-flex mt-3 d-none">
-                <img src="@/assets/images/socials/twitter.svg" alt="Twitter" />
-                <img
-                  src="@/assets/images/socials/linkedin.svg"
-                  alt="Linkedin"
-                />
-                <img src="@/assets/images/socials/github.svg" alt="Github" />
-                <img
-                  src="@/assets/images/socials/instagram.svg"
-                  alt="Instagram"
-                />
-              </div>
-            </div>
-          </slide>
-        </carousel>
-      </div> -->
       <VueSlickCarousel v-bind="settings">
         <div class="team__card">
           <img
@@ -179,7 +37,7 @@
         <div class="team__card">
           <img
             src="@/assets/images/team/piyush.png"
-            alt="Ankit"
+            alt="Piyush"
             class="pr__img"
           />
           <h4 class="pr__name mt-4">Piyush</h4>
@@ -194,7 +52,7 @@
         <div class="team__card">
           <img
             src="@/assets/images/team/santosh.png"
-            alt="Ankit"
+            alt="Santosh"
             class="pr__img"
           />
           <h4 class="pr__name mt-4">Santosh</h4>
@@ -209,7 +67,7 @@
         <div class="team__card">
           <img
             src="@/assets/images/team/aditya.png"
-            alt="Ankit"
+            alt="Aditya"
             class="pr__img"
           />
           <h4 class="pr__name mt-4">Aditya</h4>
