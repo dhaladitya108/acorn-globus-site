d765f3540537da5c3f169094e157bbc60b473ca7
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2022-10-27 16:50:14 +0530

    blog page

---
+++ components/BlogCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="d-flex flex-column flex-md-row gap-md-4 gap-3">
+    <img src="@/assets/images/blog/abstract1.png" alt="Abstract Image" />
+    <div class="d-flex flex-column">
+      <div class="d-flex align-items-center gap-3">
+        <span class="badge">{{ article.topic }}</span>
+        <span class="sm__text">{{ article.timeToRead }}</span>
+      </div>
+      <h2 class="blog__heading my-2">
+        {{ article.title }}
+      </h2>
+      <h3 class="name__text mt-2">{{ article.author }}</h3>
+      <span class="sm__text">{{ article.date }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["article"],
+};
+</script>
+
+<style scoped>
+.badge {
+  font-size: 14px;
+  line-height: 21px;
+  color: var(--clr-primary);
+  padding: 4px 8px;
+  border: 1px solid var(--clr-primary);
+  text-transform: capitalize;
+}
+
+.blog__heading {
+  font-size: 28px;
+  line-height: 36px;
+  font-weight: 700;
+}
+
+.sm__text {
+  font-size: 14px;
+  line-height: 21px;
+}
+
+.name__text {
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 32px;
+  margin: 0;
+}
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div class="bg__primary">
+      <Nav />
+    </div>
+    <div class="position-relative overflow-hidden mb-5 pb-5">
+      <h1 class="big__heading my-20 text-center">Our Blog</h1>
+      <div class="d-flex flex-column gap-16 container">
+        <BlogCard
+          v-for="article in articles"
+          :key="article.title"
+          :article="article"
+        />
+      </div>
+      <img
+        src="@/assets/images/acorn-light-watermark.png"
+        alt="Acorn Watermark"
+        class="position-absolute acorn-light-watermark"
+      />
+    </div>
+    <div class="bg__primary">
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      articles: [
+        {
+          title: "Which tech stack is best for website development?",
+          author: "Vivek Badani",
+          date: "20 Oct 2022",
+          topic: "tech",
+        },
+        {
+          title:
+            "Nicklas Haslestad on ditching PDF guidelines, the future of design & why AI isn't a threat",
+          author: "Vivek Badani",
+          date: "27 Oct 2022",
+          topic: "design",
+        },
+        {
+          title:
+            "Doing things that scare us: the things that unexpectedly build confidence",
+          author: "Vivek Badani",
+          date: "22 Oct 2022",
+          topic: "career",
+        },
+      ],
+    };
+  },
+  // async asyncData({ $content }) {
+  //   const articles = await $content("blog")
+  //     .where({
+  //       published: true,
+  //     })
+  //     .sortBy("createdAt", "desc")
+  //     .fetch();
+
+  //   return articles;
+  // },
+};
+</script>
+
+<style>
+.big__heading {
+  font-size: 4.5rem;
+  line-height: 86px;
+  font-weight: 900;
+  text-transform: capitalize;
+}
+
+.my-20 {
+  margin-block: 80px;
+}
+
+.gap-16 {
+  gap: 64px;
+}
+
+.acorn-light-watermark {
+  top: 36px;
+  z-index: -1;
+}
+
+@media screen and (max-width: 768px) {
+  .big__heading {
+    font-size: 48px;
+    line-height: 56px;
+    font-weight: 700;
+  }
+}
+</style>
