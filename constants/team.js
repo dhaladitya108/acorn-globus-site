3bb168dca6fbc38574d27a11aad17a38adb6ff3b
siddharth0807 <siddharthkumar0807@gmail.com>
2023-09-25 14:00:17 +0530

    added siddharth profile to the acorn site

--- constants/team.js
+++ constants/team.js
@@ -1,68 +1,73 @@
 // team members data
 export const teamMembers = [
-    {
-      name: "Ankit",
-      role: "Founder & Partner",
-      profileImage: "ankit",
-    },
-    {
-      name: "Tarun",
-      role: "Founder & Partner",
-      profileImage: "tarun",
-    },
-    {
-      name: "Piyush",
-      role: "Developer",
-      profileImage: "piyush",
-    },
-    {
-      name: "Santosh",
-      role: "Developer",
-      profileImage: "santosh",
-    },
-    {
-      name: "Aditya",
-      role: "Designer",
-      profileImage: "aditya",
-    },
-    {
-      name: "Anirudh",
-      role: "Full Stack Engineer",
-      profileImage: "anirudh",
-    },
-    {
-      name: "Ravindra",
-      role: "Full Stack Engineer",
-      profileImage: "ravindra",
-    },
-    {
-      name: "Deeptanshu",
-      role: "Full Stack Developer",
-      profileImage: "deeptanshu",
-    },
-    {
-      name: "Deepika",
-      role: "Business Development",
-      profileImage: "deepika",
-    },
-    {
-      name: "Navni",
-      role: "Marketing Associate",
-      profileImage: "navni",
-    },
-    {
-      name: "Neha",
-      role: "Senior Sales Manager",
-      profileImage: "neha",
-    },
-    {
-      name: "Akshat",
-      role: "Business Development Associate",
-      profileImage: "akshat",
-    },
-    {
-      name: "Dnyaneshwari",
-      role: "Sales Development Representative ",
-      profileImage: "dnyaneshwari",
-    },
-  ];
+  {
+    name: "Ankit",
+    role: "Founder & Partner",
+    profileImage: "ankit",
+  },
+  {
+    name: "Tarun",
+    role: "Founder & Partner",
+    profileImage: "tarun",
+  },
+  {
+    name: "Piyush",
+    role: "Developer",
+    profileImage: "piyush",
+  },
+  {
+    name: "Santosh",
+    role: "Developer",
+    profileImage: "santosh",
+  },
+  {
+    name: "Aditya",
+    role: "Designer",
+    profileImage: "aditya",
+  },
+  {
+    name: "Anirudh",
+    role: "Full Stack Engineer",
+    profileImage: "anirudh",
+  },
+  {
+    name: "Ravindra",
+    role: "Full Stack Engineer",
+    profileImage: "ravindra",
+  },
+  {
+    name: "Deeptanshu",
+    role: "Full Stack Developer",
+    profileImage: "deeptanshu",
+  },
+  {
+    name: "Siddharth",
+    role: "Full Stack Engineer",
+    profileImage: "siddharth",
+  },
+  {
+    name: "Deepika",
+    role: "Business Development",
+    profileImage: "deepika",
+  },
+  {
+    name: "Navni",
+    role: "Marketing Associate",
+    profileImage: "navni",
+  },
+  {
+    name: "Neha",
+    role: "Senior Sales Manager",
+    profileImage: "neha",
+  },
+  {
+    name: "Akshat",
+    role: "Business Development Associate",
+    profileImage: "akshat",
+  },
+  {
+    name: "Dnyaneshwari",
+    role: "Sales Development Representative ",
+    profileImage: "dnyaneshwari",
+  },
+];
