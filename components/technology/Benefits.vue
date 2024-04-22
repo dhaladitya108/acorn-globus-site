5ed80006a4ec08d0b737ab60dfd43be03327e94f
aniketsat <aniketsatapathy193@gmail.com>
2024-04-22 21:12:24 +0530

    Technology Page Basic Layout

---
+++ components/technology/Benefits.vue
@@ -0,0 +1,20 @@
+<template>
+  <section id="benefits">
+    <h2 class="subheading">Benefits of ReactJS Web Development</h2>
+
+    <div>
+      <div></div>
+      <img src="~/assets/images/content.png" alt="Content">
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+#benefits {
+  text-align: center;
+}
+</style>
