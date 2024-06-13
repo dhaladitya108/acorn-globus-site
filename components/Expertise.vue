8d0951c02fb4598c8bb0c8f58f8548774ab258e7
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2024-06-13 09:02:35 +0530

    style: update styling in Expertise component

--- components/Expertise.vue
+++ components/Expertise.vue
@@ -1,8 +1,8 @@
 <template>
-  <section id="expertize" class="expertize">
+  <section id="expertize" class="wrapper">
     <div class="container mx-auto px-4 md:px-12 text-center">
-      <h2 class="subheading">Our Industry Expertise</h2>
-      <div class="expertises">
+      <h2 class="section__heading">Our Industry Expertise</h2>
+      <div class="expertises mt-14">
         <div class="expertise">
           <img
             src="~/assets/images/expertise/healthcare.png"
@@ -36,15 +36,14 @@
 <style scoped>
 .expertises {
   width: 100%;
-  margin: 64px auto 0;
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 48px;
-  align-self: stretch;
   flex-wrap: wrap;
   padding: 0;
 }
+
 .expertise {
   display: flex;
   width: 160px;
@@ -55,16 +54,19 @@
   align-items: center;
   gap: 24px;
 }
+
 .expertise:hover {
   border-radius: 8px;
   background: white;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.05), 0 2px 4px 0 rgba(0, 0, 0, 0.07);
 }
+
 .expertise img {
   width: 64px;
   height: 64px;
   aspect-ratio: 1/1;
 }
+
 @media screen and (max-width: 768px) {
   .expertises {
     gap: 16px;
@@ -73,6 +75,7 @@
 
 @media screen and (max-width: 576px) {
   .expertises {
+    display: grid;
     gap: 8px;
     margin: 32px auto 0;
     padding: 0 32px;
