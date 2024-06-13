46012b5ea268dd52ee664d58390f5c56126efd07
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2024-06-14 01:04:23 +0530

    refactor(Project): add ProjectCard and updated Project component styles

--- components/Project.vue
+++ components/Project.vue
@@ -1,303 +1,62 @@
 <template>
-  <section id="projects">
+  <section id="projects" class="wrapper">
     <div class="container__wrapper pb-3">
-      <span class="subtext">our projects</span>
-      <h2 class="subheading mb-5">Few Projects we worked on</h2>
-      <div class="projects">
-        <div class="row1">
-          <div class="row1__content">
-            <div class="heading row1_heading">
-              <img
-                src="~/assets/images/projects/cayzen-logo.svg"
-                alt="cayzen-logo"
-              />
-              <h3>Cayzen</h3>
-            </div>
-            <p class="text">
-              Cayzen is a communication tool for managers and frontline workers.
-              It builds on the concept of Kaizen and help companies build
-              product that are driven by the people who are on the frontline.
-            </p>
-            <div class="row1__content__cta">
-              <a href="https://eitoss.com/" target="_blank">
-                View Project
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="20"
-                  viewBox="0 0 24 20"
-                  fill="none"
-                >
-                  <path
-                    d="M22.9024 11.2535C23.4251 10.7552 23.4448 9.92743 22.9464 9.40474L14.8249 0.887051C14.3265 0.364364 13.4988 0.344656 12.9761 0.843032C12.4534 1.34141 12.4337 2.16915 12.9321 2.69183L20.1512 10.2631L12.5799 17.4822C12.0572 17.9806 12.0375 18.8083 12.5359 19.331C13.0343 19.8537 13.862 19.8734 14.3847 19.3751L22.9024 11.2535ZM0.968874 11.1144L21.9689 11.6144L22.0311 8.99983L1.03113 8.49983L0.968874 11.1144Z"
-                    fill="#192A52"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div class="row1__image">
-            <img src="~/assets/images/projects/cayzen.png" alt="Cayzen" />
-          </div>
-        </div>
-        <div class="row2">
-          <div class="row2__project">
-            <div class="row2__project__image">
-              <img src="~/assets/images/projects/rumie.png" alt="Rumie" />
-            </div>
-            <div class="row2__project__content">
-              <div class="heading">
-                <img
-                  src="~/assets/images/projects/rumie-logo.png"
-                  alt="rumie-logo"
-                />
-                <h3>Rumie</h3>
-              </div>
-              <p class="text">
-                Medify is a platform that connects patients with doctors. It
-                helps patients to book appointments with doctors and get
-                consultation online.
-              </p>
-              <a
-                href="https://about.rumie.org/"
-                target="_blank"
-                class="row2__project__content__cta"
-              >
-                View Project
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="20"
-                  viewBox="0 0 24 20"
-                  fill="none"
-                >
-                  <path
-                    d="M22.9024 11.2535C23.4251 10.7552 23.4448 9.92743 22.9464 9.40474L14.8249 0.887051C14.3265 0.364364 13.4988 0.344656 12.9761 0.843032C12.4534 1.34141 12.4337 2.16915 12.9321 2.69183L20.1512 10.2631L12.5799 17.4822C12.0572 17.9806 12.0375 18.8083 12.5359 19.331C13.0343 19.8537 13.862 19.8734 14.3847 19.3751L22.9024 11.2535ZM0.968874 11.1144L21.9689 11.6144L22.0311 8.99983L1.03113 8.49983L0.968874 11.1144Z"
-                    fill="#192A52"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div class="row2__project">
-            <div class="row2__project__image">
-              <img
-                src="~/assets/images/projects/formester.png"
-                alt="Formester"
-              />
-            </div>
-            <div class="row2__project__content">
-              <div class="heading">
-                <img
-                  src="~/assets/images/projects/formester-logo.png"
-                  alt="formester-logo"
-                />
-                <h3>Formester</h3>
-              </div>
-              <p class="text">
-                A form backend as a service. It makes creating form endpoints
-                for websites very easy. It requires zero programming knowledge.
-                The user has the full control over the look and feel of the
-                form. In Formester validation, spam protection and integration
-                with different CRMs are few clicks away.
-              </p>
-              <a
-                href="https://formester.com/"
-                target="_blank"
-                class="row2__project__content__cta"
-              >
-                View Project
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="20"
-                  viewBox="0 0 24 20"
-                  fill="none"
-                >
-                  <path
-                    d="M22.9024 11.2535C23.4251 10.7552 23.4448 9.92743 22.9464 9.40474L14.8249 0.887051C14.3265 0.364364 13.4988 0.344656 12.9761 0.843032C12.4534 1.34141 12.4337 2.16915 12.9321 2.69183L20.1512 10.2631L12.5799 17.4822C12.0572 17.9806 12.0375 18.8083 12.5359 19.331C13.0343 19.8537 13.862 19.8734 14.3847 19.3751L22.9024 11.2535ZM0.968874 11.1144L21.9689 11.6144L22.0311 8.99983L1.03113 8.49983L0.968874 11.1144Z"
-                    fill="#192A52"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
+      <div class="text-center">
+        <span class="subtext">our projects</span>
+        <h2 class="section__heading mb-5">Few Projects we worked on</h2>
+      </div>
+      <div class="flex flex-col gap-8 mt-6">
+        <ProjectCard
+          v-for="project in projects"
+          :key="project.title"
+          v-bind="project"
+        />
       </div>
     </div>
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+import cayzenImage from "~/assets/images/projects/cayzen.png";
+import cayzenLogo from "~/assets/images/projects/cayzen-logo.svg";
+import rumieImage from "~/assets/images/projects/rumie.png";
+import rumieLogo from "~/assets/images/projects/rumie-logo.png";
+import formesterImage from "~/assets/images/projects/formester.png";
+import formesterLogo from "~/assets/images/projects/formester-logo.png";
+
+const projects = [
+  {
+    title: "Cayzen",
+    description:
+      "Cayzen is a communication tool for managers and frontline workers. It builds on the concept of Kaizen and help companies build product that are driven by the people who are on the frontline.",
+    image: cayzenImage,
+    logo: cayzenLogo,
+    tags: ["design services", "web development", "strategy"],
+  },
+  {
+    title: "Rumie",
+    description:
+      "Rumie is an open community of motivated learners, thousands of amazing volunteers, a technology ecosystem but also a nonprofit.",
+    image: rumieImage,
+    logo: rumieLogo,
+    tags: ["design services", "web development", "strategy"],
+    imgPosition: "right",
+  },
+  {
+    title: "Formester",
+    description:
+      "A form backend as a service. It makes creating form endpoints for websites very easy. It requires zero programming knowledge. The user has the full control over the look and feel of the form. In Formester validation, spam protection and integration with different CRMs are few clicks away.",
+    image: formesterImage,
+    logo: formesterLogo,
+    tags: ["design services", "web development", "strategy"],
+  },
+];
+</script>
 
 <style>
 .projects {
   display: flex;
   flex-direction: column;
   gap: 4rem;
-}
-.row1 {
-  display: flex;
-  flex-direction: row;
-  gap: 2rem;
-}
-.row1__content {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  width: 50%;
-  gap: 4px;
-}
-.row1__content__cta {
-  margin: 0;
-  padding: 0;
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  color: var(--clr-primary #192a52);
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-  text-decoration-line: underline;
-  text-transform: capitalize;
-}
-.row1__content__cta a {
-  padding: 0;
-  display: flex;
-  flex-direction: row;
-  gap: 0.5rem;
-  align-items: flex-start;
-  justify-content: center;
-  color: black;
-  transition: all ease-in-out 300ms;
-}
-.row1__content__cta a:hover {
-  gap: 1rem;
-}
-.heading {
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-}
-.heading > img {
-  width: 30px;
-  height: auto;
-  margin-right: 1rem;
-}
-.row1_heading > img {
-  width: 42px;
-  height: auto;
-  margin-right: 1rem;
-}
-.heading h3 {
-  font-size: 2.5rem;
-  font-weight: 700;
-  color: #192a52;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: auto;
-}
-.row1__image {
-  display: flex;
-  padding: 102px 58px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 8px;
-  background: #f0f7f9;
-}
-.row1__image > img {
-  width: 100%;
-  height: auto;
-}
-
-.row2 {
-  display: flex;
-  flex-direction: row;
-  gap: 2rem;
-}
-.row2__project {
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-}
-.row2__project__image {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 8px;
-  background: #f0f7f9;
-}
-.row2__project__content {
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-  align-items: flex-start;
-}
-.row2__project__content__cta {
-  margin: 0;
-  padding: 0;
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  gap: 0.5rem;
-  color: var(--clr-primary #192a52);
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-  text-decoration-line: underline;
-  text-transform: capitalize;
-  transition: all ease-in-out 300ms;
-}
-.row2__project__content__cta:hover {
-  gap: 1rem;
-  color: var(--clr-primary #192a52);
-}
-.row2__project__content h3 {
-  font-size: 2rem;
-  font-weight: 700;
-  color: #192a52;
-}
-
-@media screen and (max-width: 768px) {
-  .row1 {
-    flex-direction: column-reverse;
-    gap: 0;
-  }
-  .row1__content {
-    width: 100%;
-    gap: 4px;
-    margin: 1.75rem 0 0;
-  }
-  .row1__image {
-    padding: 0;
-  }
-  .row1__image > img {
-    width: 100%;
-    height: auto;
-  }
-  .row2 {
-    flex-direction: column;
-  }
-  .row2__project {
-    flex-direction: column;
-  }
-  .row2__project__content {
-    gap: 4px;
-  }
-  .row2__project__content .text {
-    margin-top: 4px;
-  }
-  .row2__project__image {
-    padding: 0;
-  }
-  .row2__project__image > img {
-    width: 100%;
-    height: auto;
-  }
 }
 </style>
---
+++ components/project/ProjectCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="project__card grid grid-cols-1 lg:grid-cols-2 p-6 rounded-lg">
+    <div
+      class="bg-primary-light p-10 rounded-lg"
+      :class="{ 'lg:order-last': imgPosition === 'right' }"
+    >
+      <img :src="image" alt="Cayzen" class="rounded-xl" />
+    </div>
+    <div
+      class="flex flex-col justify-center items-center lg:items-start gap-4 p-12"
+    >
+      <div class="flex gap-2 items-center">
+        <img :src="logo" alt="cayzen-logo" />
+        <h3 class="text-3xl font-semibold">{{ title }}</h3>
+      </div>
+      <p class="text-center lg:text-start">
+        {{ description }}
+      </p>
+      <div class="flex flex-wrap justify-center lg:justify-start gap-3">
+        <div
+          v-for="tag in tags"
+          key="tag"
+          class="bg-primary-light py-2 px-4 rounded-3xl uppercase text-nowrap text-sm font-medium"
+        >
+          {{ tag }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  logo: String,
+  title: String,
+  description: String,
+  image: String,
+  tags: Array,
+  imgPosition: {
+    type: String,
+    default: "left",
+    validator: (value) => {
+      return ["left", "right"].includes(value.toLowerCase());
+    },
+  },
+});
+</script>
+
+<style scoped>
+.project__card:hover {
+  box-shadow: 0px 4px 18.3px 0px rgba(0, 0, 0, 0.08),
+    0px 2px 4px 0px rgba(0, 0, 0, 0.07);
+}
+</style>
