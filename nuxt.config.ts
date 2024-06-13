406d760c7261ffe806e6f1c246a41cbb71279350
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2024-06-13 09:59:42 +0530

    chore: remove meta from nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,18 +7,18 @@ export default defineNuxtConfig({
   // generate: {
   //     fallback: true
   // },
-  app: {
-    head: {
-      title: "Acorn Globus",
-      meta: [
-        ...meta,
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { hid: "description", name: "description", content: "" },
-        { name: "format-detection", content: "telephone=no" },
-      ],
-    },
-  },
+  // app: {
+  //   head: {
+  //     title: "Acorn Globus",
+  //     meta: [
+  //       ...meta,
+  //       { charset: "utf-8" },
+  //       { name: "viewport", content: "width=device-width, initial-scale=1" },
+  //       { hid: "description", name: "description", content: "" },
+  //       { name: "format-detection", content: "telephone=no" },
+  //     ],
+  //   },
+  // },
   components: true,
   css: [
     // "~/assets/css/bootstrap.min.css",
