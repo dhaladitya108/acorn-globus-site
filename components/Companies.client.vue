6bbea2742b97acc8d84602136a5225f2d9c23afc
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2024-06-14 09:38:24 +0530

    feat: companies auto carousel

--- components/Companies.client.vue
+++ components/Companies.client.vue
@@ -1,9 +1,63 @@
 <template>
   <section id="companies" class="companies">
     <div class="container__wrapper">
-      <div
-        class="flex flex-col sm:flex-row items-center justify-between flex-wrap"
-      >
+      <Vue3Marquee v-if="isMobile">
+        <div class="box">
+          <a href="https://formester.com/" target="_blank">
+            <img
+              src="~/assets/images/companies/formester.png"
+              alt="Formester logo"
+              class="company__img"
+            />
+          </a>
+        </div>
+        <div class="box">
+          <a href="https://about.rumie.org/" target="_blank">
+            <img
+              src="~/assets/images/companies/rumie.png"
+              alt="Rumie logo"
+              class="company__img rumie"
+            />
+          </a>
+        </div>
+        <div class="box">
+          <a href="https://www.mayple.com/main/b" target="_blank">
+            <img
+              src="~/assets/images/companies/mayple.png"
+              alt="Mayple logo"
+              class="company__img mayple"
+            />
+          </a>
+        </div>
+        <div class="box">
+          <a href="https://eitoss.com/" target="_blank">
+            <img
+              src="~/assets/images/companies/cayzen.png"
+              alt="Cayzen logo"
+              class="company__img cayzen"
+            />
+          </a>
+        </div>
+        <div class="box">
+          <a href="https://www.hackerrank.com/" target="_blank">
+            <img
+              src="~/assets/images/companies/hacker-rank.png"
+              alt="HackerRank logo"
+              class="company__img hacker-rank"
+            />
+          </a>
+        </div>
+        <div class="box">
+          <a href="https://equipmentconnect.co.uk/" target="_blank">
+            <img
+              src="~/assets/images/companies/equipment-connect.png"
+              alt="Equipment connect logo"
+              class="company__img equipment-connect"
+            />
+          </a>
+        </div>
+      </Vue3Marquee>
+      <div v-else class="flex items-center">
         <div class="box">
           <a href="https://formester.com/" target="_blank">
             <img
@@ -63,7 +117,18 @@
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+const isMobile = ref(false);
+
+onMounted(() => {
+  isMobile.value = window.innerWidth < 1024;
+  window.addEventListener("resize", handleResize);
+});
+
+const handleResize = () => {
+  isMobile.value = window.innerWidth < 1024;
+};
+</script>
 
 <style>
 .companies {
@@ -97,8 +162,6 @@
   }
 
   .company__img {
-    filter: none;
-    opacity: 1;
     transition: 0.2s ease;
   }
 }
