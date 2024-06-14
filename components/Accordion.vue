0063ab50738b6b81d3cc2b011a6a8200a67eebb8
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2024-06-14 08:13:09 +0530

    feat: accordion component

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -1,5 +1,121 @@
-<template></template>
+<template>
+  <article class="p-5 border border-neutral-200 rounded-lg cursor-pointer">
+    <header class="flex justify-between">
+      <h4 @click="expanded = !expanded" class="text-xl font-semibold">
+        {{ title }}
+      </h4>
+      <button class="btn" @click="expanded = !expanded">
+        <svg
+          v-show="expanded"
+          class="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M20 12H4"
+          ></path>
+        </svg>
+        <svg
+          v-show="!expanded"
+          class="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          ></path>
+        </svg>
+      </button>
+    </header>
+    <div :style="contentStyle" class="content">
+      <p :style="infoStyle" class="info">{{ info }}</p>
+    </div>
+  </article>
+</template>
 
-<script setup></script>
+<script setup>
+const props = defineProps({
+  title: String,
+  info: String,
+  isOpen: {
+    type: Boolean,
+    default: false,
+  },
+});
+const expanded = ref(props.isOpen);
+const contentStyle = computed(() => {
+  return { "max-height": expanded.value ? "100px" : 0 };
+});
 
-<style scoped></style>
+const infoStyle = computed(() => {
+  return {
+    opacity: expanded.value ? 1 : 0,
+    marginTop: expanded.value ? "8px" : 0,
+  };
+});
+</script>
+
+<style scoped>
+.question {
+  padding: 1rem 1.5rem;
+  border: 2px solid var(--clr-grey-special);
+  margin-bottom: 1rem;
+  border-radius: var(--radius);
+  box-shadow: var(--light-shadow);
+}
+
+.question p {
+  color: var(--clr-grey-3);
+  margin-bottom: 0;
+  margin-top: 0.5rem;
+}
+.question header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.question header h4 {
+  margin-bottom: 0;
+}
+.btn {
+  background: transparent;
+  border-color: transparent;
+  width: 2rem;
+  height: 2rem;
+  background: var(--clr-grey-special);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  color: var(--clr-red-special);
+  cursor: pointer;
+  margin-left: 1rem;
+  align-self: center;
+  min-width: 2rem;
+  z-index: 1;
+}
+
+.question-title {
+  cursor: pointer;
+}
+
+.content {
+  max-height: 0;
+  transition: max-height 0.2s ease-out;
+}
+.info {
+  z-index: -1;
+  opacity: 0;
+  transition: opacity 0.2s ease-out;
+}
+</style>
--- components/technology/Benefits.vue
+++ components/technology/Benefits.vue
@@ -1,18 +1,37 @@
 <template>
-  <section id="benefits">
-    <h2 class="subheading">Benefits of ReactJS Web Development</h2>
-
-    <div class="body">
-      <div></div>
-      <img src="~/assets/images/content.png" alt="Content" />
+  <section id="benefits" class="wrapper mt-8">
+    <div class="container__wrapper flex flex-col gap-16">
+      <h2 class="section__heading">Benefits of ReactJS Web Development</h2>
+      <div class="flex flex-col gap-4 lg:flex-row xl:gap-8">
+        <img
+          src="~/assets/images/content.png"
+          alt="Content"
+          class="lg:order-last"
+        />
+        <div class="flex flex-col gap-4">
+          <Accordion
+            v-for="accordion in accordions"
+            :key="accordion.id"
+            v-bind="accordion"
+          />
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+const accordions = [
+  {
+    title: "Interactive and Engaging Web Apps",
+    info: "ReactJS excels at creating dynamic interfaces. Its component-based architecture allows for smooth transitions and updates, keeping your users engaged and providing a delightful user experience.",
+    isOpen: true,
+  },
+  {
+    title: "Easy to Learn and Use",
+    info: "ReactJS is known for its simplicity and ease of use. It allows developers to create complex applications with minimal effort, making it a popular choice for web development.",
+  },
+];
+</script>
 
-<style scoped>
-#benefits {
-  text-align: center;
-}
-</style>
+<style scoped></style>
